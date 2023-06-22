92443ee1425dca18e1eb0e4d5b2e23d41eb224dd
arcy405 <arcy405@gmail.com>
2023-06-22 21:14:04 +0545

    imported csv

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+
 class Item:
     pay_rate = 0.8  # the oay rate after 20% discount
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
 
@@ -12,53 +15,76 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
-item1 = Item("phone", 100 )
-# item1.name = "phone"
-# item1.price = 100
-# item1.quantity = 5
-# print(item1.calculate_total_price(item1.price, item1.quantity))
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
 
+    def __repr__(self):
+        return f"Archana('{self.name}, {self.price}, {self.quantity}')"
 
-item2 = Item("laptop", 2000, 4)
-item2.has_numpad = True
-# item2.name = "laptop"
-# item2.price = 2000
-# item2.quantity = 2
-# print(item2.calculate_total_price(item2.price, item2.quantity))
-
-
-# print(type(item1))
-# print(type(item1.name))
-
-# print(item1.name)
-# print(item2.name)
-# print(item1.price)
-# print(item2.price)
-# print(item1.quantity)
-# print(item2.quantity)
-# print(item2.has_numpad)
-
-# print(item1.calculate_total_price())
-# print(item2.calculate_total_price() )
-
-# print(Item.pay_rate)
-# print(item1.pay_rate)
-# print(item2.pay_rate)
+# item1 = Item("phone", 100 )
+# # item1.name = "phone"
+# # item1.price = 100
+# # item1.quantity = 5
+# # print(item1.calculate_total_price(item1.price, item1.quantity))
 #
-print(Item.__dict__)
-print(item1.__dict__)
-print(item2.__dict__)
+#
+# item2 = Item("laptop", 2000, 4)
+# item2.has_numpad = True
+# # item2.name = "laptop"
+# # item2.price = 2000
+# # item2.quantity = 2
+# # print(item2.calculate_total_price(item2.price, item2.quantity))
+#
+#
+# # print(type(item1))
+# # print(type(item1.name))
+#
+# # print(item1.name)
+# # print(item2.name)
+# # print(item1.price)
+# # print(item2.price)
+# # print(item1.quantity)
+# # print(item2.quantity)
+# # print(item2.has_numpad)
+#
+# # print(item1.calculate_total_price())
+# # print(item2.calculate_total_price() )
+#
+# # print(Item.pay_rate)
+# # print(item1.pay_rate)
+# # print(item2.pay_rate)
+# #
+# print(Item.__dict__)
+# print(item1.__dict__)
+# print(item2.__dict__)
+#
+# item1.apply_discount()
+# print(item1.price)
+#
+# item2 = Item("Laptop", 1000, 3)
+# item2.pay_rate = 0.7
+# item2.apply_discount()
+# print(item2.price)
 
-item1.apply_discount()
-print(item1.price)
 
-item2 = Item("Laptop", 1000, 3)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+# item1 = Item("Phone", 100, 1)
+# item2 = Item("Laptop", 1000, 3)
+# item3 = Item("Cable", 10, 5)
+# item4 = Item("Mouse", 50, 5)
+# item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
+
+for item in Item.all:
+    print(item.name)
